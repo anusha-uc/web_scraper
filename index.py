640c42705017813b58f68a094a9ee5d2b33802e8
anushamanoj <71338009+anushamanoj@users.noreply.github.com>
2020-09-18 10:56:03 +0530

    initial8

--- index.py
+++ index.py
@@ -22,8 +22,8 @@ class Index():
         return self.mycursor
     
     #fetches query
-    def fetch_fromDB(self,query):
-        self.mycursor.execute(query)
+    def fetch_fromDB(self,select_query):
+        self.mycursor.execute(select_query)
         return self.mycursor.fetchall()
 
 def main():
@@ -36,33 +36,31 @@ def main():
     parser.add_argument('pages',type = int,help = 'Number of pages to fetch')
     args = parser.parse_args()
 
-    #creating empty dictionary
-    author_dict = {}   
-    tag_dict = {}
-    quote_dict = {}
-    quoteTag_dict = {}
-
     #stores value from database to dictionary
-    query = "SELECT * FROM author"
-    store_result = index_obj.fetch_fromDB(query)
+    select_query = "SELECT * FROM author"
+    store_result = index_obj.fetch_fromDB(select_query)
+    author_dict = {}  
     for row in store_result:
         author_dict[row[2]] = [row[0],row[1],row[3]]         
 
     #stores value from database to dictionary
-    query = "SELECT * FROM tag"
-    store_result = index_obj.fetch_fromDB(query)
+    select_query = "SELECT * FROM tag"
+    store_result = index_obj.fetch_fromDB(select_query)
+    tag_dict = {}
     for row in store_result:
         tag_dict[row[1]] = row[0]                     
     
     #stores value from database to dictionary                                                
-    query = "SELECT * FROM quotes"
-    store_result = index_obj.fetch_fromDB(query)
+    select_query = "SELECT * FROM quotes"
+    store_result = index_obj.fetch_fromDB(select_query)
+    quote_dict = {}
     for row in store_result:
         quote_dict[row[1]] = [row[0],row[2]]
 
     #stores value from database to dictionary
-    query = "SELECT * FROM quote_tag"
-    store_result = index_obj.fetch_fromDB(query)
+    select_query = "SELECT * FROM quote_tag"
+    store_result = index_obj.fetch_fromDB(select_query)
+    quoteTag_dict = {}
     for row in store_result:
         if row[0] in quoteTag_dict:
             quoteTag_dict[row[0]].append(row[1])
@@ -85,13 +83,13 @@ def main():
     #getting response from the urllib2 request
     response = urllib2.urlopen(Scrape_URL)            
     html_contents = response.read()           
-    beautifulSoup_obj = BeautifulSoup(html_contents, 'html.parser') #creates beautifulSoup_obj object from html content
+    beautifulSoup_obj = BeautifulSoup(html_contents, 'html.parser') 
 
     #takes total page number in the argument 
     page_count = args.pages
 
     #looping through number of page
-    for page_no in range(1,page_count+1):
+    for page_no in range(1, page_count + 1):
         for quote_div in beautifulSoup_obj.find_all('div',class_ = 'quote'): 
             author_name = quote_div.find('small',class_ = 'author').text
             author_id = scrapeobj.getAuthorId(author_name) 
