d203566b32a80112787dfc0832db8a298d3c0827
anushamanoj <71338009+anushamanoj@users.noreply.github.com>
2020-09-18 12:17:26 +0530

    initial8

--- index.py
+++ index.py
@@ -12,6 +12,10 @@ class Index():
         connectionDB_obj = connection.ConnectionDB()
         self.mydb = connectionDB_obj.connect_db()             
         self.mycursor = self.mydb.cursor()
+        #argparse
+        parser = argparse.ArgumentParser(description = "Find pages")
+        parser.add_argument('pages',type = int,help = 'Number of pages to fetch')
+        self.pages = parser.parse_args().pages
     
     #returns mydb object
     def get_mydb(self):
@@ -26,98 +30,89 @@ class Index():
         self.mycursor.execute(select_query)
         return self.mycursor.fetchall()
 
-def main():
-    index_obj = Index()
-    mydb = index_obj.get_mydb()
-    mycursor = index_obj.get_mycursor()
+    def index_main(self):
+        mydb = self.get_mydb()
+        mycursor = self.get_mycursor()
 
-    #argparse
-    parser = argparse.ArgumentParser(description = "Find pages")
-    parser.add_argument('pages',type = int,help = 'Number of pages to fetch')
-    args = parser.parse_args()
+        #stores value from database to dictionary
+        select_query = "SELECT * FROM author"
+        store_result = self.fetch_fromDB(select_query)
+        author_dict = {}  
+        for row in store_result:
+            author_dict[row[2]] = [row[0],row[1],row[3]]         
 
-    #stores value from database to dictionary
-    select_query = "SELECT * FROM author"
-    store_result = index_obj.fetch_fromDB(select_query)
-    author_dict = {}  
-    for row in store_result:
-        author_dict[row[2]] = [row[0],row[1],row[3]]         
+        #stores value from database to dictionary
+        select_query = "SELECT * FROM tag"
+        store_result = self.fetch_fromDB(select_query)
+        tag_dict = {}
+        for row in store_result:
+            tag_dict[row[1]] = row[0]                     
+        
+        #stores value from database to dictionary                                                
+        select_query = "SELECT * FROM quotes"
+        store_result = self.fetch_fromDB(select_query)
+        quote_dict = {}
+        for row in store_result:
+            quote_dict[row[1]] = [row[0],row[2]]
 
-    #stores value from database to dictionary
-    select_query = "SELECT * FROM tag"
-    store_result = index_obj.fetch_fromDB(select_query)
-    tag_dict = {}
-    for row in store_result:
-        tag_dict[row[1]] = row[0]                     
-    
-    #stores value from database to dictionary                                                
-    select_query = "SELECT * FROM quotes"
-    store_result = index_obj.fetch_fromDB(select_query)
-    quote_dict = {}
-    for row in store_result:
-        quote_dict[row[1]] = [row[0],row[2]]
+        #stores value from database to dictionary
+        select_query = "SELECT * FROM quote_tag"
+        store_result = self.fetch_fromDB(select_query)
+        quoteTag_dict = {}
+        for row in store_result:
+            if row[0] in quoteTag_dict:
+                quoteTag_dict[row[0]].append(row[1])
+            else:                        
+                quoteTag_dict[row[0]] = [row[1]]                
 
-    #stores value from database to dictionary
-    select_query = "SELECT * FROM quote_tag"
-    store_result = index_obj.fetch_fromDB(select_query)
-    quoteTag_dict = {}
-    for row in store_result:
-        if row[0] in quoteTag_dict:
-            quoteTag_dict[row[0]].append(row[1])
-        else:                        
-            quoteTag_dict[row[0]] = [row[1]]                
+        #create class object
+        scrapeobj = scrape.Scrape(quote_dict,author_dict,tag_dict,quoteTag_dict) 
+        
+        #fetching root URL
+        Scrape_URL = 'http://toscrape.com' 
+        response = urllib2.urlopen(Scrape_URL)            
+        html_contents = response.read()           
+        beautifulSoup_obj = BeautifulSoup(html_contents, 'html.parser')     
 
-    #create class object
-    scrapeobj = scrape.Scrape(quote_dict,author_dict,tag_dict,quoteTag_dict) 
-    
-    #fetching root URL
-    Scrape_URL = 'http://toscrape.com' 
-    response = urllib2.urlopen(Scrape_URL)            
-    html_contents = response.read()           
-    beautifulSoup_obj = BeautifulSoup(html_contents, 'html.parser')     
-
-    #finds "A website" in all anchor tag and saves in the root URL
-    anchor_tag=beautifulSoup_obj.find('a',text = re.compile('A website')) 
-    Scrape_URL = anchor_tag['href']
-    
-
-    #making the request 
-    response = urllib2.urlopen(Scrape_URL)            
-    html_contents = response.read()           
-    beautifulSoup_obj = BeautifulSoup(html_contents, 'html.parser') 
-
-    #takes total page number in the argument 
-    page_count = args.pages
-
-    #looping through number of page
-    for page_no in range(1, page_count + 1):
-        for quote_div in beautifulSoup_obj.find_all('div',class_ = 'quote'): 
-            author_name = quote_div.find('small',class_ = 'author').text
-            author_id = scrapeobj.getAuthorId(author_name) 
-            #checks if author_id exists
-            if not author_id:
-                anchor_tag=quote_div.find('a', {'href': re.compile(r'^\/author\/')})
-                Scrape_URL = "http://quotes.toscrape.com" + anchor_tag['href'] 
-                response = urllib2.urlopen(Scrape_URL)
-                html_contents = response.read()
-                author_beautifulSoup_obj = BeautifulSoup(html_contents, 'html.parser') 
-                name = author_beautifulSoup_obj.find('h3',class_ = 'author-title').text
-                desc = author_beautifulSoup_obj.find('div',class_ = 'author-description').text
-                date = author_beautifulSoup_obj.find('span',class_ = 'author-born-date').text
-                author_id = scrapeobj.author(name,desc,date)
-
-            quote_id = scrapeobj.quote(quote_div.find('span',class_ = 'text').text,author_id)
-            tags = quote_div.find_all('a',class_ = 'tag')
-            for tag in tags:    
-                tag_id = scrapeobj.tags_function(tag.text)
-                scrapeobj.qoute_tag(quote_id,tag_id)
-
-        #increments the page number
-        Scrape_URL = "http://quotes.toscrape.com/page/" + str(page_no + 1)     
-        response = urllib2.urlopen(Scrape_URL)           
-        html_contents = response.read()          
+        #finds "A website" in all anchor tag and saves in the root URL
+        anchor_tag=beautifulSoup_obj.find('a',text = re.compile('A website')) 
+        Scrape_URL = anchor_tag['href']
+        
+        #making the request 
+        response = urllib2.urlopen(Scrape_URL)            
+        html_contents = response.read()           
         beautifulSoup_obj = BeautifulSoup(html_contents, 'html.parser') 
-    mydb.commit()
+
+        #looping through number of page
+        for page_no in range(1, self.pages + 1):
+            for quote_div in beautifulSoup_obj.find_all('div',class_ = 'quote'): 
+                author_name = quote_div.find('small',class_ = 'author').text
+                author_id = scrapeobj.getAuthorId(author_name) 
+                #checks if author_id exists
+                if not author_id:
+                    anchor_tag=quote_div.find('a', {'href': re.compile(r'^\/author\/')})
+                    Scrape_URL = "http://quotes.toscrape.com" + anchor_tag['href'] 
+                    response = urllib2.urlopen(Scrape_URL)
+                    html_contents = response.read()
+                    author_beautifulSoup_obj = BeautifulSoup(html_contents, 'html.parser') 
+                    name = author_beautifulSoup_obj.find('h3',class_ = 'author-title').text
+                    desc = author_beautifulSoup_obj.find('div',class_ = 'author-description').text
+                    date = author_beautifulSoup_obj.find('span',class_ = 'author-born-date').text
+                    author_id = scrapeobj.author(name,desc,date)
+
+                quote_id = scrapeobj.quote(quote_div.find('span',class_ = 'text').text,author_id)
+                tags = quote_div.find_all('a',class_ = 'tag')
+                for tag in tags:    
+                    tag_id = scrapeobj.tags_function(tag.text)
+                    scrapeobj.qoute_tag(quote_id,tag_id)
+
+            #increments the page number
+            Scrape_URL = "http://quotes.toscrape.com/page/" + str(page_no + 1)     
+            response = urllib2.urlopen(Scrape_URL)           
+            html_contents = response.read()          
+            beautifulSoup_obj = BeautifulSoup(html_contents, 'html.parser') 
+        mydb.commit()
 
 if __name__ == "__main__":
-    main()
+    index_obj=Index()
+    index_obj.index_main()
