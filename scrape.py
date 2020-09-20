3953298b6135a50eb37290e86a1329d4cde8e347
anushamanoj <71338009+anushamanoj@users.noreply.github.com>
2020-09-20 12:44:08 +0530

    initial9

--- scrape.py
+++ scrape.py
@@ -13,33 +13,33 @@ class Scrape(ConnectionDB):
         # stores value from database to dictionary
         select_query = "SELECT * FROM author"
         store_result = self.fetch_fromDB(select_query)
-        self.authors = {}
+        self.author_dict = {}
         for row in store_result:
-            self.authors[row[2]] = [row[0], row[1], row[3]]
+            self.author_dict[row[2]] = [row[0], row[1], row[3]]
 
         # stores value from database to dictionary
         select_query = "SELECT * FROM tag"
         store_result = self.fetch_fromDB(select_query)
-        self.tags = {}
+        self.tag_dict = {}
         for row in store_result:
-            self.tags[row[1]] = row[0]
+            self.tag_dict[row[1]] = row[0]
 
         # stores value from database to dictionary
         select_query = "SELECT * FROM quotes"
         store_result = self.fetch_fromDB(select_query)
-        self.quotes = {}
+        self.quote_dict = {}
         for row in store_result:
-            self.quotes[row[1]] = [row[0], row[2]]
+            self.quote_dict[row[1]] = [row[0], row[2]]
 
         # stores value from database to dictionary
         select_query = "SELECT * FROM quote_tag"
         store_result = self.fetch_fromDB(select_query)
-        self.qoute_tags = {}
+        self.quoteTag_dict = {}
         for row in store_result:
-            if row[0] in self.qoute_tags:
-                self.qoute_tags[row[0]].append(row[1])
+            if row[0] in self.quoteTag_dict:
+                self.quoteTag_dict[row[0]].append(row[1])
             else:
-                self.qoute_tags[row[0]] = [row[1]]
+                self.quoteTag_dict[row[0]] = [row[1]]
 
       # fetches query
     def fetch_fromDB(self, select_query):
@@ -53,48 +53,48 @@ class Scrape(ConnectionDB):
         self.mycursor.execute(
             "insert into author (name,Description,DOB) values(%s,%s,%s)", (name, description, dob))
         author_id = self.mycursor.lastrowid
-        self.authors[name] = [author_id, description, dob]
+        self.author_dict[name] = [author_id, description, dob]
         self.mydb.commit()
         return author_id
 
     # returns author id
     def getAuthorId(self, name):
-        if name in self.authors:
-            return self.authors[name][0]
+        if name in self.author_dict:
+            return self.author_dict[name][0]
         else:
             return False
 
     # checks for existing quotes and then inserts into table
     def quote(self, quote, author_id):
-        if quote in self.quotes:
-            return self.quotes[quote][0]
+        if quote in self.quote_dict:
+            return self.quote_dict[quote][0]
         self.mycursor.execute(
             "INSERT INTO quotes (quote,author_id) VALUES (%s,%s)", (quote, author_id))
         quote_id = self.mycursor.lastrowid
-        self.quotes[quote] = [quote_id, author_id]
+        self.quote_dict[quote] = [quote_id, author_id]
         self.mydb.commit()
         return quote_id
 
     # checks for tag id. if exists, inserts into tag table
     def tags_function(self, tag):
-        if tag in self.tags:
-            return self.tags[tag]
+        if tag in self.tag_dict:
+            return self.tag_dict[tag]
         else:
             self.mycursor.execute("insert into tag (name) values(%s)", (tag,))
             tag_id = self.mycursor.lastrowid
-            self.tags[tag] = tag_id
+            self.tag_dict[tag] = tag_id
             self.mydb.commit()
             return tag_id
 
     # checks for quote id and tag id and inserts
     def qoute_tag(self, qoute_id, tag_id):
-        if qoute_id in self.qoute_tags:
-            if tag_id not in self.qoute_tags[qoute_id]:
+        if qoute_id in self.quoteTag_dict:
+            if tag_id not in self.quoteTag_dict[qoute_id]:
                 self.mycursor.execute(
                     "insert into quote_tag (q_id,t_id) values(%s,%s)", (qoute_id, tag_id))
-                self.qoute_tags[qoute_id].append(tag_id)
+                self.quoteTag_dict[qoute_id].append(tag_id)
         else:
-            self.qoute_tags[qoute_id] = [tag_id]
+            self.quoteTag_dict[qoute_id] = [tag_id]
             self.mycursor.execute(
                 "insert into quote_tag (q_id,t_id) values(%s,%s)", (qoute_id, tag_id))
         self.mydb.commit()
